6fb5bad64ad832079249c1430d81c13adb049242
hyunsoo <hermeto@atg.co.kr>
2024-01-22 13:26:23 +0900

    1강완료

--- src/App.js
+++ src/App.js
@@ -12,8 +12,12 @@ function App() {
     "파이썬독학",
   ]);
   let [따봉, 따봉변경] = useState([0, 0, 0]);
-
   let [modal, setModal] = useState(false);
+  let [order, setOrder] = useState(0);
+
+  let [입력값, 입력값변경] = useState(null);
+
+  let [날짜, 날짜변경] = useState(["2월 15일", "2월 16일", "2월 17일"]);
 
   //모든게 state가 아니라 자주변경하는건 state로 해주는거다.
   //고정적인 제목같은거 차라리 그냥 하드코딩이 이득이다.
@@ -89,11 +93,14 @@ function App() {
             <h4
               onClick={() => {
                 setModal(true);
+                setOrder(i);
               }}
             >
               {글제목[i]}
+
               <span
-                onClick={() => {
+                onClick={(e) => {
+                  e.stopPropagation();
                   let copy = [...따봉];
                   copy[i] = copy[i] + 1;
                   따봉변경(copy);
@@ -103,12 +110,68 @@ function App() {
               </span>
               {따봉[i]}
             </h4>
-            <p>2월 17일 발행</p>
+            <p>{날짜[i]}</p>
+
+            <button
+              onClick={() => {
+                let copy = [...글제목];
+                copy.splice(i, 1);
+                글제목변경(copy);
+
+                let copy2 = [...따봉];
+                copy2.splice(i, 1);
+                따봉변경(copy2);
+              }}
+            >
+              삭제
+            </button>
           </div>
         );
       })}
+
+      <input
+        onChange={(e) => {
+          입력값변경(e.target.value);
+        }}
+      ></input>
+      <button
+        onClick={(e) => {
+          // let copy = [...글제목];
+          // copy.push(입력값);
+          // 글제목변경(copy);
+
+          // let copy2 = [...따봉];
+          // copy2.push(0);
+          // 따봉변경(copy2);
+          입력값 !== null && 입력값 !== ""
+            ? // 입력값이 null이 아닌 경우
+              (() => {
+                let currentDate = new Date();
+
+                let copy = [...글제목];
+                copy.push(입력값);
+                글제목변경(copy);
+
+                let copy2 = [...따봉];
+                copy2.push(0);
+                따봉변경(copy2);
+
+                let copy3 = [...날짜];
+                copy3.push(currentDate.toString());
+                날짜변경(copy3);
+
+                // 입력값을 null로 초기화
+                입력값변경(null);
+
+                console.log("입력값 :" + 입력값);
+              })()
+            : e.preventDefault();
+        }}
+      >
+        글발행
+      </button>
       {modal === true ? (
-        <Modal 글제목={글제목} 글제목변경={글제목변경} />
+        <Modal 글제목={글제목} 글제목변경={글제목변경} order={order} />
       ) : null}
     </div>
   );
@@ -117,7 +180,7 @@ function App() {
 function Modal(props) {
   return (
     <div className="modal">
-      <h4>{props.글제목[0]}</h4>
+      <h4>{props.글제목[props.order]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
       <button
