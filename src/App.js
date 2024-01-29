5be3c3d1971c96066f88282dffa6519c55c38f63
hyunsoo <hermeto@atg.co.kr>
2024-01-29 17:25:17 +0900

    얕은복사/깊은복사 예시

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import { useState } from "react";
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable no-undef */
+import { useState, useEffect } from "react";
 import "./App.css";
+import _ from "lodash";
 
 function App() {
   let post = "역삼 우동 맛집";
@@ -7,9 +10,11 @@ function App() {
   //document.querySelector('h4').innerHTML = post;
 
   let [글제목, 글제목변경] = useState([
-    "남자코트추천",
-    "강남 우동맛집",
-    "파이썬독학",
+    "기본탄탄",
+    "개념탄탄",
+    "응용하기",
+    // [1, 2, 3],
+    // { key: "value" },
   ]);
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
@@ -18,6 +23,11 @@ function App() {
   let [입력값, 입력값변경] = useState(null);
 
   let [날짜, 날짜변경] = useState(["2월 15일", "2월 16일", "2월 17일"]);
+
+  //let [test, setTest] = useState(["하나", "둘", [1, 2, 3], { key: "value" }]);
+  let test = [["하나", "둘", [1, 2, 3], { key: "value" }]];
+
+  let test2 = ["하나", "둘", "셋"];
 
   //모든게 state가 아니라 자주변경하는건 state로 해주는거다.
   //고정적인 제목같은거 차라리 그냥 하드코딩이 이득이다.
@@ -29,6 +39,10 @@ function App() {
   // let c = num[1];
   //let [a, c] = [1, 2];
 
+  // useEffect(() => {
+  //   console.log("test : ", test);
+  // }, [test]);
+
   return (
     <div className="App">
       <div className="black-nav">
@@ -38,17 +52,55 @@ function App() {
         onClick={() => {
           let copy = [...글제목];
           copy[0] = "테스트";
+          // copy[3] = [9, 9, 9];
+          // copy[4] = { key: "test" };
           //copy.sort();
           글제목변경(copy);
+
+          //주소값이 같아서 상태관리 비교했을 때 값이 같아서 렌더링이 일어나지 않음
+          // let temp = 글제목;
+          // temp[0] = "테스트";
+          // 글제목변경(temp);
+
+          //얕은복사
+          let copy2 = [...test];
+          copy2[0][1] = "불나방";
+          copy2[0][2][0] = 999;
+          copy2[1] = "하이";
+          copy2[2] = [9, 9, 9];
+          copy2[3] = { confirm: "hi" };
+
+          let copy3 = [...test2];
+          copy3[0] = "백";
+
+          //깊은복사
+          // let copy2 = _.cloneDeep(test);
+          // copy2[0][1] = "불나방";
+          // copy2[0][2][0] = 999;
+          // copy2[1] = "하이";
+          // copy2[2] = [9, 9, 9];
+          // copy2[3] = { confirm: "hi" };
+
+          //setTest(copy2);
+
+          // console.log("test : ", test);
+          console.log("copy2 : ", copy2);
+          console.log("test : ", test);
+
+          // console.log("글제목 : ", 글제목);
+          // console.log("copy : ", copy);
+
+          // console.log("test2 : ", test2);
+          // console.log("copy3 : ", copy3);
         }}
       >
-        가나다순정렬
+        제목변경
       </button>
 
       <button
         onClick={() => {
           let copy = [...글제목];
-          copy[0] = "여자코트 추천";
+          copy[0] = "중급개념";
           글제목변경(copy);
         }}
       >
